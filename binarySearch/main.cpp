8f3d580b15606f64c4ad7c446fda4eb564818a92
Andre <thompson.andre.m@gmail.com>
2023-03-31 13:21:13 -0400

    binary search algo

--- binarySearch/main.cpp
+++ binarySearch/main.cpp
@@ -1,53 +1,46 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
-#include <fstream>
 
-void fillVector(std::vector<std::string>& toFill, std::string line){
+int binarySearch(char list[], int size, char key){
+    int high, low, mid;
+    low = 0;
+    high = size - 1;
 
-
-
-
+    //halfs array until there is no mid point
+    while(high >= low){
+        //mid == middle element
+        mid = (high + low) / 2;
+        //if key is less than middle element, start next iteration at middle of first half
+        if(list[mid] < key){
+            low = mid + 1;
+        //if key is greater than middle element, start next iteration at middle of second half
+        } else if (list[mid] > key){
+            high = mid - 1;
+        //if key is equal to middle element return element number of the key in array
+        } else {
+            return mid;
+        }
+    }
+    //if two elements left and neither are the key return -1 (key not found)
+    return -1;
 }
-
-
-
-
 
 
 int main(){
-    //in and out files
-    std::ifstream file;
-    std::ofstream out;
-    
-    std::string fileName;
-    std::vector<std::string> list;
-    int x, key, index;
-    
-    std::cout << "Input file name (file.txt): ";
-    std::cin >> fileName;
-
-    file.open(fileName);
-    out.open("output.txt");
-
-
-    if(!file.is_open()){
-        std::cout << "Error: Input file failed to open";
-        return 1;
-    } else if (!out.is_open()){
-        std::cout << "Error: Failed to create output file";
-        return 1;
+    //sorted list
+    char list[] = {'a', 'b', 'c'}, key;
+    int size = 3;
+    int element = 0;
+    for(int i = 0; i < 3; i++){
+        std::cout << list[i] << "\n";
     }
-
-    
-    while(!file.eof()){
-        std::string currentLine;
-        file >> currentLine;
-        fillVector(list, currentLine);
-
+    std::cout << "Please enter search key: ";
+    std::cin >> key;
+    element = binarySearch(list, size, key);
+    if(element == -1){
+        std::cout << "Key not found\n";
+        return 0;
+    } else {
+        std::cout << "Key found at element: " << element << "\n";
     }
-    
-   
-
 }
-
